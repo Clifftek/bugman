806e6962c63db39da46453a6dc456e6ef7110458
Jakob Johnson <jakob.johnson.dev@gmail.com>
2022-03-13 19:25:14 +1100

    added PWA functionality

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: [],
   },
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
